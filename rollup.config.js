0964e178ac5937a63191d5da30363f4c4ea5735f
Trezy <tre@trezy.com>
2020-07-05 14:06:45 -0500

    build(rollup): add multiple builds for size comparison

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import { sizeSnapshot } from 'rollup-plugin-size-snapshot'
 import { terser } from 'rollup-plugin-terser'
+import closureCompile from 'rollup-plugin-closure-compile'
 import notify from 'rollup-plugin-notify'
 import progress from 'rollup-plugin-progress'
 import visualizer from 'rollup-plugin-visualizer'
@@ -15,7 +16,52 @@ export default [
       {
         file: '0.js',
         format: 'cjs',
+      },
+      {
+        file: 'builds/terser.js',
+        format: 'cjs',
         plugins: [
+          terser(),
+        ],
+      },
+      {
+        file: 'builds/closure.js',
+        format: 'cjs',
+        plugins: [
+          closureCompile(),
+        ],
+      },
+      {
+        file: 'builds/closure-terser.js',
+        format: 'cjs',
+        plugins: [
+          closureCompile(),
+          terser(),
+        ],
+      },
+      {
+        file: 'builds/terser-closure.js',
+        format: 'cjs',
+        plugins: [
+          terser(),
+          closureCompile(),
+        ],
+      },
+      {
+        file: 'builds/closure-terser-closure.js',
+        format: 'cjs',
+        plugins: [
+          closureCompile(),
+          terser(),
+          closureCompile(),
+        ],
+      },
+      {
+        file: 'builds/terser-closure-terser.js',
+        format: 'cjs',
+        plugins: [
+          terser(),
+          closureCompile(),
           terser(),
         ],
       },

a1b6daacc43278c5f0dca3f4bb5ef662c53e4034
Trezy <tre@trezy.com>
2020-08-14 12:28:06 -0500

    Add gzip compression and a dev server

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,12 @@
 import { sizeSnapshot } from 'rollup-plugin-size-snapshot'
 import { terser } from 'rollup-plugin-terser'
 import closureCompile from 'rollup-plugin-closure-compile'
+import dev from 'rollup-plugin-dev'
 import htmlTemplate from 'rollup-plugin-generate-html-template'
 import notify from 'rollup-plugin-notify'
 import progress from 'rollup-plugin-progress'
 import visualizer from 'rollup-plugin-visualizer'
+import gzip from 'rollup-plugin-gzip'
 
 
 
@@ -24,10 +26,16 @@ export default [
             template: 'src/index.html',
             target: 'index.html',
           }),
+          gzip(),
         ],
       },
     ],
     plugins: [
+      dev({
+        dirs: [
+          'dist',
+        ],
+      }),
       progress(),
       sizeSnapshot(),
       visualizer({

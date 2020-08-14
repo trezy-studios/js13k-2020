fa6fc98ab1902f966e99731e4c8fd27d77ba58be
Trezy <tre@trezy.com>
2020-08-14 12:23:23 -0500

    Inject bundle `<script>` tag into html

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import { sizeSnapshot } from 'rollup-plugin-size-snapshot'
 import { terser } from 'rollup-plugin-terser'
 import closureCompile from 'rollup-plugin-closure-compile'
+import htmlTemplate from 'rollup-plugin-generate-html-template'
 import notify from 'rollup-plugin-notify'
 import progress from 'rollup-plugin-progress'
 import visualizer from 'rollup-plugin-visualizer'
@@ -14,11 +15,15 @@ export default [
     input: 'src/index.js',
     output: [
       {
-        file: '0.js',
+        file: 'dist/0.js',
         format: 'iife',
         plugins: [
           closureCompile(),
           terser(),
+          htmlTemplate({
+            template: 'src/index.html',
+            target: 'index.html',
+          }),
         ],
       },
     ],

48b13548ca5f17e4744c01b6897e150c19a15542
Trezy <tre@trezy.com>
2020-08-19 00:18:52 -0500

    add base CSS

--- src/index.js
+++ src/index.js
@@ -1,24 +1,29 @@
+import './index.css'
 // import { grid } from './state.js'
 // import { updateTile } from './helpers/updateTile.js'
 // updateTile(3, 4, 2)
-// console.log(grid);
+// console.log(grid)
 
 
 
-import "./patches/addEventListener";
-import { canvas } from "./render/canvas";
+import "./patches/addEventListener"
+import { canvas } from "./render/canvas"
 
 
 
-const render = canvas(document.querySelector("canvas"));
-let box_x = 100;
-let frame = 0;
-setInterval(() => {
-    box_x += Math.sin(frame / 180 * Math.PI);
-    frame++;
-    render.color("red", "red");
-    render.rect(0, 0, innerWidth, innerHeight);
-    render.color("black", "black");
-    render.rect(box_x - 10, 0, 20, 20);
-    render.update();
-});
+const render = canvas(document.querySelector("canvas"))
+let box_x = 100
+let frame = 0
+
+function gameLoop () {
+	box_x += Math.sin(frame / 180 * Math.PI)
+	frame++
+	render.color("red", "red")
+	render.rect(0, 0, innerWidth, innerHeight)
+	render.color("black", "black")
+	render.rect(box_x - 10, 0, 20, 20)
+	render.update()
+	requestAnimationFrame(gameLoop)
+}
+
+gameLoop()

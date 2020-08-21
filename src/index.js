20c6620a4ce06bee061a5a67510334f9dd2fb0ee
Trezy <tre@trezy.com>
2020-08-20 22:32:41 -0500

    add basic menus

--- src/index.js
+++ src/index.js
@@ -12,6 +12,8 @@ import * as maps from './maps/index'
 const canvasElement = document.querySelector('canvas')
 const canvasHeight = canvasElement.height
 const canvasWidth = canvasElement.width
+const mainMenuElement = document.querySelector('#main')
+const mapSelectMenuElement = document.querySelector('#map-select')
 const render = canvas(canvasElement)
 
 
@@ -19,13 +21,16 @@ const render = canvas(canvasElement)
 
 
 // Local variables
-let currentMap = 'test'
 let frame = 0
 
 
 
 
 
+const mapSelect = () => {
+	mainMenuElement.style.display = 'none'
+	mapSelectMenuElement.style.display = 'flex'
+}
 
 const gameLoop = () => {
 	frame++
@@ -35,6 +40,27 @@ const gameLoop = () => {
 
 	requestAnimationFrame(gameLoop)
 }
-render.map = maps[value]
 
-gameLoop()
+const handleMapButtonClick = ({ target: { value } }) => {
+	render.map = maps[value]
+	mapSelectMenuElement.style.display = 'none'
+	canvasElement.style.display = 'flex'
+	gameLoop()
+}
+
+const createMapButton = mapName => {
+	const mapButton = document.createElement('button')
+	mapButton.setAttribute('type', 'button')
+	mapButton.setAttribute('value', mapName)
+	mapButton.on('click', handleMapButtonClick)
+	mapButton.innerHTML = mapName
+	mapSelectMenuElement.appendChild(mapButton)
+}
+
+const initialize = () => {
+	mainMenuElement.style.display = 'flex'
+	document.querySelector('#start').on('click', mapSelect)
+	Object.keys(maps).forEach(createMapButton)
+}
+
+initialize()

7d7af62e0c47f69d264a620cfdd77585dd341f19
Trezy <tre@trezy.com>
2020-09-02 14:51:07 -0500

    remove zzfx

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ import {
 } from './helpers/controls'
 import { createStringCanvas } from './render/font'
 import { Screen } from './structures/Screen'
-import { settings } from './helpers/settings'
+import { settings } from './data/settings'
 import { state } from './data/state'
 import { updateGameScale } from './helpers/updateGameScale'
 import * as maps from './maps/index'
@@ -50,8 +50,6 @@ let music = null
 // screens
 let loadingScreen = new Screen({
 	async onInit() {
-		console.log('loading')
-
 		// Load the "LOADING" image so it displays before we start loading anything
 		// else.
 		let loadingElement = document.querySelector('#loading')
@@ -118,7 +116,7 @@ let settingsScreen = new Screen({
 
 				switch (type) {
 					case 'checkbox':
-						settings[name] = checked
+						settings[name] = +checked
 						break
 
 					case 'number':
@@ -132,7 +130,6 @@ let settingsScreen = new Screen({
 
 			let settingsValue = settings[name]
 
-			console.log({inputElement})
 			if (typeof settingsValue === 'boolean') {
 				inputElement.checked = settingsValue
 			} else if (inputElement.type === 'radio') {

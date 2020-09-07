623518bd6fe2663fc012f3355518dbc8e1d27d2e
Trezy <tre@trezy.com>
2020-09-07 01:05:39 -0500

    add animated exit

--- src/data/entities.js
+++ src/data/entities.js
@@ -8,16 +8,21 @@ import { TILE_SIZE } from '../data/grid'
 
 
 export let entities = {
-	robot(context, offsetX, offsetY) {
-		let framerate = 30
+	exit(context, offsetX, offsetY) {
+		let frames = 10
 		let x = TILE_SIZE.w * offsetX
 		let y = TILE_SIZE.h * offsetY
-		let sourceX = 8
-
-		if ((state.frame % framerate) > (framerate / 2)) {
-			sourceX += 8
-		}
+		let sourceX = 24 + (Math.floor((state.frame % 60) / (60 / frames)) * 8)
 
 		context.image(spritesheetImage, sourceX, 0, 8, 16, x, y - 13, 8, 16)
-	}
+	},
+
+	robot(context, offsetX, offsetY) {
+		let frames = 2
+		let x = TILE_SIZE.w * offsetX
+		let y = TILE_SIZE.h * offsetY
+		let sourceX = 8 + (Math.floor((state.frame % 60) / (60 / frames)) * 8)
+
+		context.image(spritesheetImage, sourceX, 0, 8, 16, x, y - 13, 8, 16)
+	},
 }

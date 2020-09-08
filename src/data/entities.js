e89bd381de0b666c03adbdf165ab1d1fc966d25c
IanSSenne <IanSSenne@gmail.com>
2020-09-08 10:39:47 -0700

    its alive!

--- src/data/entities.js
+++ src/data/entities.js
@@ -18,10 +18,8 @@ export let entities = {
 
 	robot(context, e) {
 		let frames = 2
-		let x = TILE_SIZE.w * e.x
-		let y = TILE_SIZE.h * e.y
 		let robot_state = e.state;
-		if (!robot_state.path || robot_state.path.length === 0) {
+		if (!robot_state.path || robot_state.path.length === 0 && e.x === robot_state.next.x && robot_state.next.y === e.y) {
 			let potential = state.map.path({ x: e.x, y: e.y }, { x: -1, y: -1 });
 			let end = state.entities.find(ent => ent.type === "exit");
 			let spot = potential.find(({ x, y }) => x == end.x && end.y == y)
@@ -31,13 +29,40 @@ export let entities = {
 				spot = rightmost[Math.floor(Math.random() * rightmost.length)];
 			}
 			robot_state.path = state.map.path({ x: e.x, y: e.y }, spot);
-			// console.log(robot_state);
-		} else {
-			let next = robot_state.path.shift();
-			e.x = next.x;
-			e.y = next.y;
+			robot_state.next = robot_state.path.shift() || e;
+			robot_state.ox = 0;
+			robot_state.oy = 0;
 		}
+
+		if (state.frame % 8 == 0) {
+			if (
+				e.x + robot_state.ox / TILE_SIZE.w == robot_state.next.x &&
+				e.y + robot_state.oy / TILE_SIZE.h == robot_state.next.y
+			) {
+				e.x = robot_state.next.x;
+				e.y = robot_state.next.y;
+				robot_state.next = robot_state.path.shift() || e;
+				robot_state.ox = 0;
+				robot_state.oy = 0;
+			}
+			if (robot_state.next.x > e.x) {
+				robot_state.ox++;
+			}
+			if (robot_state.next.x < e.x) {
+				robot_state.ox--;
+			}
+			if (robot_state.next.y > e.y) {
+				robot_state.oy++;
+			}
+			if (robot_state.next.y < e.y) {
+				robot_state.oy--;
+			}
+		}
+
+
+		let x = TILE_SIZE.w * e.x
+		let y = TILE_SIZE.h * e.y
 		let sourceX = 8 + (Math.floor((state.frame % 60) / (60 / frames)) * 8)
-		context.image(spritesheetImage, sourceX, 0, 8, 16, x, y - 13, 8, 16)
+		context.image(spritesheetImage, sourceX, 0, 8, 16, robot_state.ox + x, robot_state.oy + y - 13, 8, 16)
 	},
 }

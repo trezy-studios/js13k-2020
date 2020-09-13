0b2101b8a27b642dfdb98d27637f945515fc1fe3
Trezy <tre@trezy.com>
2020-09-13 01:44:58 -0500

    add second tutorial level

--- src/maps/index.js
+++ src/maps/index.js
@@ -1,5 +1,6 @@
 // Local imports
-import m100 from '../../maps/1xx.json'
+import m01 from '../../maps/01.json'
+import m02 from '../../maps/02.json'
 import boop from '../../maps/Boop.json'
 import crosses from '../../maps/Crosses.json'
 import labyrinth from '../../maps/Labyrinth.json'
@@ -7,7 +8,8 @@ import test from '../../maps/Test.json'
 import zuri from '../../maps/Zuri.json'
 
 export let maps = [
-	m100,
+	m01,
+	m02,
 	boop,
 	test,
 	zuri,

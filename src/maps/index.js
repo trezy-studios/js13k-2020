ff8683cd7c59fcea4d2d32e89b6f8ea67cc1a299
Trezy <tre@trezy.com>
2020-09-13 05:11:17 -0500

    update map filenames

--- src/maps/index.js
+++ src/maps/index.js
@@ -2,19 +2,23 @@
 import m01 from '../../maps/01.json'
 import m02 from '../../maps/02.json'
 import m03 from '../../maps/03.json'
-import boop from '../../maps/Boop.json'
-import crosses from '../../maps/Crosses.json'
-import labyrinth from '../../maps/Labyrinth.json'
-import test from '../../maps/Test.json'
-import zuri from '../../maps/Zuri.json'
+import m04 from '../../maps/04.json'
+import m05 from '../../maps/05.json'
+import m06 from '../../maps/06.json'
+import m07 from '../../maps/07.json'
+import m08 from '../../maps/08.json'
+import m09 from '../../maps/09.json'
+import m10 from '../../maps/10.json'
 
 export let maps = [
 	m01,
 	m02,
 	m03,
-	boop,
-	test,
-	zuri,
-	crosses,
-	labyrinth,
+	m04,
+	m05,
+	m06,
+	m07,
+	m08,
+	m09,
+	m10,
 ]
